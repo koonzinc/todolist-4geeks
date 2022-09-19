e8ab82a09cec608fdec2ee98c486ed82979e2101
koonzinc <koonz.eth@gmail.com>
2022-09-19 14:00:18 -0400

    finish

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Todo from './Todo';
 import TodoForm from './TodoForm';
 
 const TodoList = () => {
@@ -14,10 +15,28 @@ const TodoList = () => {
         setTodos(newTodos);
     }
 
+    const removeTodo = (id) => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    }
+
+    const completeTodo = (id) => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos);
+    }
+
   return (
-    <div>
-        <h1>What needs to get done?</h1>
+    <div className='todo-list-container'>
+        <h1 className='todo-list-header'>What needs to get done?</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+        {todos.length === 0 ? <p>Add a todo</p> : <p>{todos.length} todos left</p>}
     </div>
   )
 }
