9bd30dc0e2c4ca715efb0f07d8d78bb403e17a02
koonzinc <koonz.eth@gmail.com>
2022-09-19 12:27:50 -0400

    fix

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,12 +2,22 @@ import React, {useState} from 'react';
 import TodoForm from './TodoForm';
 
 const TodoList = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos);
+    }
 
   return (
     <div>
         <h1>What needs to get done?</h1>
-        <TodoForm />
+        <TodoForm onSubmit={addTodo} />
     </div>
   )
 }
